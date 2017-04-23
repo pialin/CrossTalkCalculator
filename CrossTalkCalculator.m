3b08ce67ab361f2355e18108bfec03f464581e8c
pialin <765422908@qq.com>
2017-04-23 18:26:46 +0800

    use regexp to parse text data file(textscan used before) and delete FindFirstPatternInTextFile.m

--- CrossTalkCalculator.m
+++ CrossTalkCalculator.m
@@ -57,71 +57,78 @@ for iDataFile = 1:NumDataFile
     %当前处理数据文件的文件名
     DataFilePath = fullfile(DataFileList(iDataFile).folder,DataFileList(iDataFile).name);
    
+    %读取文件内容
+    DataFileContent = fileread(DataFilePath);
+    
+    %匹配一个数值得正则表达式
+    RegExpNumber = '[\+\-]?[0-9]*\.?[0-9]+(?:[Ee][\+\-]?[0-9]+)?';
+
+    
     %获取文本文件中的GridSize信息（即XY轴格点数目）
-    GridSize = FindFirstPatternInTextFile(DataFilePath,'Grid Size is %d x %d %*[^\n]');
+    GridSize = regexp(DataFileContent,['Grid Size is (',RegExpNumber,') x ',...
+        '(',RegExpNumber,')[ \t\r\f]*\n'],'tokens');
     %如果未找到GridSize信息则报错
     if isempty(GridSize)
         error(['无法从',DataFilePath,'找到GridSize信息']);
+    %如果找到多个GridSize信息
+    elseif numel(GridSize) > 1
+        %输出警告并选取最后一个目标
+        warning('找到多于1个GridSize信息，最靠后的一个将被用于后续计算');
+        GridSize = GridSize(end);
     end
-    %如果能找到GridSize信息，进一步获取X，Y轴方向的GridSize
-    GridSizeX =  GridSize{1};
-    GridSizeY =  GridSize{2};
-        
-    %获取文本文件中的DataCell大小信息（即每个小格的长宽尺寸）
-    DataCell = FindFirstPatternInTextFile(DataFilePath,'Data Cells are %f64 x %f64 %*[^\n]');
-    %如果未找到DataCell信息则报错
-    if isempty(DataCell)
-        error(['无法从',DataFilePath,'找到DataCell信息']);
+    %如果能找到GridSize信息，进一步获取X，Y轴方向的GridSize（regexp获取的信息是字符串类型的，需要转换成数值型）
+    GridSizeY =  str2double(GridSize{1}{1});
+    GridSizeX =  str2double(GridSize{1}{2});
+   
+    %获取数据文件中的4个边角的坐标
+    CornerXY = regexp(DataFileContent,['Corners of Data Set \(plotted\)[ \t\r\f]*\n',...
+        repmat(['[ \t]*(\w+):\((',RegExpNumber,'),(',RegExpNumber,'),',RegExpNumber,'\)[ \t\r\f]*\n'],[1,4])],...
+        'tokens');
+    %如果未找到边角坐标信息则报错
+    if isempty(CornerXY)
+        error(['无法从',DataFilePath,'找到边角坐标信息']);
+    %如果找到多个边角坐标信息
+    elseif numel(CornerXY) > 1
+        %输出警告并选取最后一个目标
+        warning('找到多于1个边角坐标信息，最靠后的一个将被用于后续计算');
+        CornerXY = CornerXY(end);
     end
-    %如果能找到DataCell信息，进一步获取X，Y轴方向的DataCell
-    DataCellX =  DataCell{1};
-    DataCellY =  DataCell{2};
-    
-    %获取文本文件中的4个角的坐标
-    [~,DataCornerStartLine] = FindFirstPatternInTextFile(DataFilePath,'Corners of Data Set (plotted)%[^\n]');
-    %如果未找到4个角的坐标则报错
-    if DataCornerStartLine == -1
-        error(['无法从',DataFilePath,'找到4个角坐标']);
+    %如果能找到边角坐标信息，将获取的token进行reshape便于观察和下一步处理
+    CornerXY = reshape(CornerXY{1},3,[])';
+    %分别获取文本文件中的4个角的坐标
+    for iCorner = 1:size(CornerXY,1)
+        switch  lower(CornerXY{iCorner,1})
+            case 'topleft'
+                TopLeftCornerXY =  str2double(CornerXY(iCorner,2:3));
+            case 'topright'
+                TopRightCornerXY =  str2double(CornerXY(iCorner,2:3));
+            case 'bottomleft'
+                BottomLeftCornerXY =  str2double(CornerXY(iCorner,2:3));
+            case 'bottomright'
+                BottomRightCornerXY =  str2double(CornerXY(iCorner,2:3));
+            otherwise
+                error(['未知的边角坐标信息:',CornerXY{iCorner,1},'(',CornerXY{iCorner,2},',',CornerXY{iCorner,3},')']);
+        end
+    end
+   
+    %判断是否得到了全部4个角坐标
+    if ~(exist('TopLeftCornerXY','var') && exist('TopRightCornerXY','var') &&...
+        exist('BottomLeftCornerXY','var') && exist('BottomRightCornerXY','var'))
+        %如果没有得到全部4个角的坐标,则抛出相应错误
+        error('无法得到全部4个角的坐标!');
     end
     
-    %打开数据文件
-    DataFileId = fopen(DataFilePath);
-    
-    DataCornerText = textscan(DataFileId,'%[^\n]',4,...
-        'HeaderLines', DataCornerStartLine, 'CollectOutput', 1);
-    
-    DataCornerText = [DataCornerText{1}{:}];
-    
-    TopLeft = textscan(DataFileId,'TopLeft:(%f64,%f64 %*[^\n]',1,...
-        'HeaderLines', DataCornerStartLine, 'CollectOutput', 1);
-    
-    
-    %计算数据的X值和Y值
-    DataX = double(0:GridSizeX-1) * DataCellX;
-    DataY = double(0:GridSizeY-1) * DataCellY;
+    %计算二维格点的XY坐标
+    DataX = linspace(TopLeftCornerXY(1),TopRightCornerXY(1),GridSizeX);
+    DataY = linspace(TopLeftCornerXY(2),BottomLeftCornerXY(2),GridSizeY);
     
     %获取文本文件中的二维数据矩阵
-    [~,MatDataStartLine] = FindFirstPatternInTextFile(DataFilePath,[repmat('%f64\t',[1,GridSizeX]),'%*[^\n]']);
-    %如果未找到二维数据矩阵则报错
-    if MatDataStartLine == -1
-        error(['无法从',DataFilePath,'找到二维矩阵数据']);
-    end
-    
-    %重新将搜索位置设置到文件开头（因为上一次的textscan会改动指向位置）
-    frewind(DataFileId);
-    
-    %根据上面得到二维矩阵开始的行数读取二维矩阵数据
-    MatDataCell = textscan(DataFileId,[repmat('%f64 ',[1,GridSizeX]),'%*[^\n]'],...
-        'HeaderLines', MatDataStartLine - 1, 'CollectOutput', 1);
-    
-    %关闭打开的数据文件
-    fclose(DataFileId);
-    
-    %将二维矩阵数据由cell类型转换为矩阵类型
-    DataZ = cell2mat(MatDataCell);
+    DataZ = regexp(DataFileContent,[repmat(['(',RegExpNumber,')\t'],[1,GridSizeX]),'[ \t\r\f]*\n'],'tokens');
+    %将数据从字符串cell类型转换成数值型矩阵
+    DataZ = reshape(str2double([DataZ{:}]),GridSizeY,[]);
     
     %当得到的二维矩阵尺寸与GridSize指定的不一致时显示错误
-    if any(size(DataZ) ~= [GridSizeX, GridSizeY]) 
+    if any(size(DataZ) ~= [GridSizeY, GridSizeX]) 
         error('得到的二维矩阵尺寸与GridSize指定的不一致!');
     end
     
@@ -133,11 +140,32 @@ for iDataFile = 1:NumDataFile
         'DataX', DataX, 'DataY', DataY, 'DataZ', DataZ);
     
 end
-save(fullfile(DataDirectory,'AllData.mat'),'DataStruct');
+%将得到的结构体数组存为Mat文件
+%生成存储文件路径
+AllDataMatPath = fullfile(DataDirectory,'AllData.mat');
+%检查是否文件是否已经存在
+if exist(AllDataMatPath,'file')
+    %如果文件已经存在，弹出对话框询问是否替换掉原有文件
+    RepalceDataMatOrNot = questdlg(['''',AllDataMatPath,'''已经存在，是否替换它？'], ...
+	'文件替换确认', ...
+	'是','否，请保留原有文件','终止程序','否，请保留原有文件');
+end
+%如果文件不存在，则直接保存文件
+if ~exist('RepalceDataMatOrNot','var') 
+    save(AllDataMatPath,'DataStruct','-mat');
+%如果文件存在，且用户选择了替换，则进行文件替换,
+elseif strcmp(RepalceDataMatOrNot,'是') 
+    save(AllDataMatPath,'DataStruct','-mat');
+    %并在命令行窗口输出提示信息
+    disp(['Info:''',AllDataMatPath,'''已被更新']);
+elseif isempty(RepalceDataMatOrNot) || strcmp(RepalceDataMatOrNot,'终止程序')
+    %终止程序
+    return;
+end
 
 
 %% 将读取到结构体的数据进行处理
-[MaxZValue,MaxZIndex] = max(DataStruct(1).DataZ(:));
+[MaxZValue, MaxZIndex] = max(DataStruct(1).DataZ(:));
 [MaxYIndex, MaxXIndex] = ind2sub(size(DataStruct(1).DataZ),MaxZIndex);
 DataZMaxY = DataStruct(1).DataZ(MaxYIndex,:);
 
@@ -147,6 +175,4 @@ for iDataFile = 1:NumDataFile
 
 end
 
-filecontent = fileread('test.txt');
-% result = regexp(filecontent,'Grid Size is ([0-9]*\.?[0-9]+) x ([0-9]*\.?[0-9]+)','tokens');
 
